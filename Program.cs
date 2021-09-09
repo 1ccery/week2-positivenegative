fd76626a1bc67d6effb79020599f62913e595631
1ccery <89841736+1ccery@users.noreply.github.com>
2021-09-09 11:20:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace positivenegative
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             //programm palub kasutajal sisetada numrit -10 kuni 10
+            //programm kontrollib numbrit
+            //kui number on positiivne, konsool kuvab positiivne
+            //kui number on negatiivne kuvab negatiivne
+            // kui number on 0 siis kuvab konsool null
+            Console.WriteLine("Palun sisestage number -10st 10neni!");
+
+
+            int Num;
+            Num = Convert.ToInt32(Console.ReadLine());
+            if (Num > 0)
+            {
+                Console.WriteLine("Positiivne");
+            }
+            else if (Num < 0)
+            {
+                Console.WriteLine("Negatiivne.");
+
+            }
+            else
+            {
+                Console.WriteLine("Null.");
+            }
+            Console.ReadLine();
+
         }
     }
 }
